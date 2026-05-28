fb064ba67d561560fb5c89fb34e2a698adbe9e4d
NicoQDS <nico.osnaghi@gmail.com>
2026-05-28 20:26:56 +0200

    Changed the way the player input the choice.
    Player input the choice pressing buttons, no longer by digiting "rock",
    "paper" or "scissor".

--- javascript.js
+++ javascript.js
@@ -2,7 +2,23 @@ let humanScore = 0
 let computerScore = 0
 let draws = 0
 let roundNum = 0
+const btnRock = document.querySelector("#rock");
+const btnPaper = document.querySelector("#paper");
+const btnScissor = document.querySelector("#scissor");
 
+// Read which button is pressed and returning the player choice
+btnRock.addEventListener("click", function (e) {
+  playRound(e.target.id)
+});
+btnPaper.addEventListener("click", function (e) {
+  playRound(e.target.id)
+});
+btnScissor.addEventListener("click", function (e) {
+  playRound(e.target.id)
+});
+
+
+// Get the player choice
 function get_pc_choice() {  // create a random choice for the PC 
     rndNumber = Math.random()
     if (rndNumber<0.3333) {
@@ -14,9 +30,10 @@ function get_pc_choice() {  // create a random choice for the PC
     }
 }
 
-function playRound() { // compare user and pc choice to determin the winner
+// Play a single round, print the result at the end
+function playRound(playerChoice) { // compare user and pc choice to determin the winner
     pc_Selec = get_pc_choice()
-    player_Selec = prompt('Make your choice!', 'PAPER').toLowerCase() // get palyer choice and make lowercase
+    player_Selec = playerChoice.toLowerCase() // get palyer choice and make lowercase
 
     game_result = "\nInvalid player input"
     win_text = "\nYou won this round, good job!"
@@ -69,6 +86,7 @@ function playRound() { // compare user and pc choice to determin the winner
     console.log(game_result, '\n' ,"\nPlayer choice: ".concat(player_Selec), "\nPC choice: ".concat(pc_Selec), '\n ','\nYour score: '.concat(humanScore),'\nComputer score: '.concat(computerScore),'\nDraws number: '.concat(draws) )
 }
 
+//Play 5 consecutive rounds, keeps track of the score, print the game result at the end
 function playGame() {
     while (roundNum<5) {
         playRound()
